12fdd8c6170fb60a34e9d8b0a066f53f2b36234b
YikeDM <dm2kbe@gmail.com>
2023-12-16 19:43:13 +0000

    javascript learning

---
+++ www/scripts/index.js
@@ -0,0 +1,9 @@
+console.log('Hello World')
+
+alert("Hello World");
+
+let boob = document.querySelector("#skills");
+boob.textContent = "Hello";
+
+let nameer = prompt("What is your name?");
+alert("Hello " + nameer);
